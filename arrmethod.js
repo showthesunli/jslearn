24226d70531ba4e172dff9d164363ae787e6ff6e
showthesunli <showthesunli@gmail.com>
2022-02-12 15:41:32 +0800

    arrMethod task-2

--- arrmethod.js
+++ arrmethod.js
@@ -24,3 +24,20 @@ function camelize(str){
 console.log(camelize("background-color"))
 console.log(camelize("list-style-image"))
 console.log(camelize("-webkit-transition"))
+
+/**
+ * 写一个函数 filterRange(arr, a, b)，该函数获取一个数组 arr，在其中查找数值大于或等于 a，且小于或等于 b 的元素，并将结果以数组的形式返回。
+
+该函数不应该修改原数组。它应该返回新的数组。
+ */
+function filterRange(arr, a, b){
+    let resArr = arr.filter((curVal, curIdx, curArr) =>{
+        return curVal >= a && curVal <=b ? true : false;
+    })
+    return resArr;
+}
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+console.log(filtered);
+console.log(arr);
