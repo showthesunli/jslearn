44d081cee8201293881e3c5afde757abbd84aded
showthesunli <showthesunli@gmail.com>
2022-02-25 17:56:30 +0800

    mixin task complete

--- class.js
+++ class.js
@@ -90,7 +90,7 @@
 
     let clock = new Clock({ template: 'h:m:s' });
     let descriptorOfstart = Object.getOwnPropertyDescriptor(clock, 'start');
-    console.log(descriptorOfstart)
+    // console.log(descriptorOfstart)
     // clock.start();
 
     class ExtendClock extends Clock {
@@ -117,7 +117,7 @@
         template: 'h:m:s',
         precision: 1000
     })
-    console.log("ec's constructor is" + (ec.constructor === ExtendClock))
+    // console.log("ec's constructor is" + (ec.constructor === ExtendClock))
     // ec.start()
 }
 
@@ -149,7 +149,7 @@
 
     }
 
-    console.log(sayMyName.name)
+    // console.log(sayMyName.name)
 
 }
 
@@ -164,13 +164,13 @@
     console.log(rabbit)
 
     let arr = [];
-    console.log(`arr\'s type is ${{}.toString.call(arr)}`)
+    // console.log(`arr\'s type is ${{}.toString.call(arr)}`)
 
-    // let user = {
-    //     [Symbol.tostringtag]:
-    // }
-    console.log(`1 \'s type is ${{}.toString.call(1.1)}`)
-    console.log(typeof 1)
+    // // let user = {
+    // //     [Symbol.tostringtag]:
+    // // }
+    // console.log(`1 \'s type is ${{}.toString.call(1.1)}`)
+    // console.log(typeof 1)
 }
 
 {
@@ -200,29 +200,121 @@
     let animal = new Animal();
     let rabbit = new Rabbit();
 
-    console.log(animal instanceof Rabbit);
+    // console.log(animal instanceof Rabbit);
 }
 {
     class User {
         fullName = 'lee yang'
 
-        sayMyName(){
+        sayMyName() {
             console.log(this.fullName);
         }
     }
 
     let user = new User();
-    console.log(user.__proto__)
-    console.log(User.fullName)
+    // console.log(user.__proto__)
+    // console.log(User.fullName)
 }
 
 {
-    class Animal{}
+    class Animal { }
 
-    class Rabbit extends Animal{}
+    class Rabbit extends Animal { }
 
     let animal = new Animal();
     let rabbit = new Rabbit();
 
-    console.log(`is animal instanceof Rabbit? ${animal instanceof Rabbit}`)
+    // console.log(`is animal instanceof Rabbit? ${animal instanceof Rabbit}`)
+}
+{
+    let user = {
+        sayMyName() {
+            console.log(`my name is ${this.name}`);
+        },
+        sayMyFirstName() {
+            console.log(`my firstName is ${this.firstName}`)
+        },
+        firstName: 'lee',
+        secondName: 'yang',
+        get name() {
+            return `${this.firstName} ${this.secondName}`
+        }
+    }
+
+    // console.log(user.sayMyName())
+}
+
+{
+    let arrFunction = function () {
+
+        console.log(this.fullName)
+    }
+
+    let user = {
+        fullName: 'leeyang',
+        arrFunction
+    }
+
+
+    // user.arrFunction()
+}
+
+{
+    let eventMixin = {
+        on(eventName, handler) {
+            let handlerArr = this.handlers?.[eventName];
+            if (!handlerArr) {
+                this.handlers[eventName] = [handler];
+            } else {
+                this.handlers[eventName].push(handler);
+            }
+        },
+        off(eventName, handler) {
+            let handlerArr = this.handlers?.[eventName];
+            if (!handlerArr) return;
+            handlerArr.splice(handlerArr.indexOf(handler), 1)
+        },
+        trigger(eventName, ...args) {
+            let handler = this.handlers?.[eventName];
+            if (handler) {
+                handler.forEach(element => {
+                    element.call(this, ...args);
+                });
+            }
+        },
+        get handlers() {
+            if (!this._handlers) {
+                this._handlers = {};
+                return this._handlers;
+            } else {
+                return this._handlers;
+            }
+        }
+
+    }
+
+    // 创建一个 class
+    class Menu {
+        choose(value) {
+            this.trigger("select", value);
+            this.trigger('delete', value)
+        }
+
+        delete(value){
+            this.trigger('delete', value)
+        }
+    }
+    // 添加带有事件相关方法的 mixin
+    Object.assign(Menu.prototype, eventMixin);
+
+    let menu = new Menu();
+
+    // 添加一个事件处理程序（handler），在被选择时被调用：
+    menu.on("select", value => console.log(`Value selected: ${value}`));
+    menu.on('delete', value => console.log(`delete value ${value}`))
+    // 触发事件 => 运行上述的事件处理程序（handler）并显示：
+    // 被选中的值：123
+    menu.choose("123");
+    menu.delete('123')
+
 }
