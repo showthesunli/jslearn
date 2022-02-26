42d666bfe46b3128cfce04a247708a96dd50a72a
showthesunli <showthesunli@gmail.com>
2022-02-27 00:10:50 +0800

    promise task done

---
+++ promise.js
@@ -0,0 +1,128 @@
+{
+    /**
+     * 内建函数 setTimeout 使用了回调函数。请创建一个基于 promise 的替代方案。
+
+     * 函数 delay(ms) 应该返回一个 promise。这个 promise 应该在 ms 毫秒后被 resolve，所以我们可以向其中添加 .then，像这样：
+     */
+
+    function delay(ms) {
+        return new Promise(function (resolve, reject) {
+            setTimeout(resolve, ms);
+        })
+    }
+    delay(10000).then(() => console.log('runs after 3 seconds'));
+}
+
+{
+    /**
+     * 重写任务 任务 "task/animate-circle-callback" 未找到 的解决方案中的 showCircle 函数，以使其返回一个 promise，而不接受回调。
+
+     * 新的用法：
+     */
+
+}
+
+{
+    // let promise = Promise.reject(new Error("Promise Failed!"));
+    // setTimeout(() => promise.catch(err => alert('caught')), 1000);
+    // Error: Promise Failed!
+    // window.addEventListener('unhandledrejection', event => alert(event.reason));
+
+    // promise.catch(err => alert('caught'))
+
+
+}
+/**
+ * function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    });
+}
+
+loadJson('no-such-user.json')
+  .catch(alert); // Error: 404
+ */
+{
+    (
+        async () => {
+            async function loadJson(url) {
+                let res = await fetch(url);
+                if (res.status == 200) {
+                    let json = await res.json();
+                    return json;
+                }
+                throw new Error(res.status);
+            }
+
+            // loadJson('no-such-user.json')
+            // .catch(console.log); // Error: 404
+
+        }
+    )()
+}
+
+{
+    (
+        async () => {
+            class HttpError extends Error {
+                constructor(response) {
+                    super(`${response.status} for ${response.url}`)
+                    this.name = this.constructor.name;
+                    this.response = response;
+                }
+            }
+
+            async function loadJson(url) {
+                let res = await fetch(url);
+                if (res.status == 200) {
+                    let json = await res.json();
+                    return json;
+                }
+                throw new HttpError(res);
+            }
+
+            async function demoGithubUser() {
+                let res;
+                while (true) {
+                    let name = prompt('Enter a name?', 'iliakan');
+                    try {
+                        res = await loadJson(`https://api.github.com/users/${name}`)
+                        console.log(res)
+                        break;
+                    } catch (error) {
+                        if (error instanceof HttpError) {
+                            console.log(`No such user, please reenter!`)
+                        } else {
+                            throw error;
+                        }
+                    }
+                }
+                return res;
+            }
+
+            // await demoGithubUser();
+        }
+    )()
+}
+
+{
+    async function wait() {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        return 10;
+    }
+
+    function f() {
+        // ……这里你应该怎么写？
+        // 我们需要调用 async wait() 并等待以拿到结果 10
+        // 记住，我们不能使用 "await"
+        wait().then((res)=>console.log(res))
+    }
+
+    f();
+}
